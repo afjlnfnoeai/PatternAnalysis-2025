686ca2b27ee0e229ddbb07b4427a08c90d9066c9
suyi <suyi.tong@student.uq.edu.au>
2025-10-16 22:41:53 +1000

    created dataset.py, path to data

---
+++ recognition/YOLOv7_LesionDetection_47609994/dataset.py
@@ -0,0 +1,10 @@
+# data loader for loading and preprocessing data
+import os
+import cv2
+
+dataset = "/Users/suyi/Desktop/3710/ISIC-2017"
+
+# test, train, val respective folders
+types = ["ISIC-2017_Test_v2_Data",
+         "ISIC-2017_Training_Data",
+         "ISIC-2017_Validation_Data"]
