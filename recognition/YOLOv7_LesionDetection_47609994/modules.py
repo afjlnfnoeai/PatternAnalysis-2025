febf134cf3493096f4176da38d2859bc2bf90ed5
suyi <suyi.tong@student.uq.edu.au>
2025-10-22 14:26:13 +1000

    yolov8

--- recognition/YOLOv7_LesionDetection_47609994/modules.py
+++
@@ -1,2 +0,0 @@
-# source code of the components of your model
-# component must be implemented as a class/function
--- recognition/YOLOv7_LesionDetection_47609994/train.py
+++
@@ -1,4 +0,0 @@
-# source code for training, val, test, saving model
-# model imp from modules.py
-# data loader imp from dataset.py
-
