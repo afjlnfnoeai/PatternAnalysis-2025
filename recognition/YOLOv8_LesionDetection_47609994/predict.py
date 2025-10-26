45799ce570daa816a8113d6fb798d21de8c97c32
suyi <suyi.tong@student.uq.edu.au>
2025-10-26 18:00:53 +1000

    load latest model helper function for predict.py

--- recognition/YOLOv8_LesionDetection_47609994/predict.py
+++ recognition/YOLOv8_LesionDetection_47609994/predict.py
@@ -4,3 +4,27 @@
 from modules import load_model
 import os
 import glob
+
+def load_latest_model():
+    """
+    finds and gets most recent trained model
+    """
+
+    training_folders = sorted(glob.glob(os.path.join("runs/train", "*")),
+                              key=os.path.getmtime,
+                              reverse=True)
+
+    if not training_folders:
+        print("no training runs yet")
+        return None
+
+    latest_run = training_folders[0]
+    best_model = os.path.join(latest_run, "weights", "best.pt")
+
+    if not os.path.exists(best_model):
+        print("not found")
+        return None
+
+    # get model
+    return load_model(best_model)
+
